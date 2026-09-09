e68b31e36e8c235ad0f84f4a051c21805d8eb8a4
toubabilisim-crypto <toubabilisim@gmail.com>
2026-09-09 22:07:35 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,7 +15,12 @@ function tarihFormatla(iso) {
 
 function saatFormatla(iso) {
   const d = new Date(iso);
-  return d.toLocaleTimeString("tr-TR", { hour: "2-digit", minute: "2-digit" });
+  return d.toLocaleString("tr-TR", {
+    day: "2-digit",
+    month: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 }
 
 function MacSatiri({ mac }) {

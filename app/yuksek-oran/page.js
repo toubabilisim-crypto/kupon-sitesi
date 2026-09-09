5c8d254295f54c31b4473f2b837c121cbe70433c
toubabilisim-crypto <toubabilisim@gmail.com>
2026-09-09 22:59:31 +0300

    Create page.js

---
+++ app/yuksek-oran/page.js
@@ -0,0 +1,8 @@
+import SayfaCekirdek from "../SayfaCekirdek";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function Page() {
+  return <SayfaCekirdek aktifYol="/yuksek-oran" />;
+}
